745b4ced85d739a1f7dae5c56c6356dfe52f8555
krsnakid <ujjwalsahu19@gmail.com>
2024-09-08 16:00:38 +0530

    commit

--- FirstCode.java
+++ FirstCode.java
@@ -2,6 +2,6 @@ public class FirstCode {
     public static void main(String[] args) {
         System.out.println("Hello, GitHub!");
         System.out.println("I am Ujjwal");
+        System.out.println("This is java language");
     }
-    
 }
