33ee04b2d228e86fd6ee140f23595b092206b62a
krsnakid <ujjwalsahu19@gmail.com>
2024-10-31 13:19:32 +0530

    final

--- FirstCode.java
+++ FirstCode.java
@@ -2,14 +2,7 @@ public class FirstCode {
     public static void main(String[] args) {
         System.out.println("Hello, GitHub!");
         System.out.println("I am Ujjwal");
-        System.out.println("This is java language");
-        System.out.println("This must be printed.");
-        System.out.println("I'm doing this from terminal.");
-        System.out.println("looking for branch");
-        System.out.println("I do not know what is happening.");
         System.out.println("Happy Diwali<3");
-        System.out.println("No Pollution");
-        System.out.println("lets inspect");
-        System.out.println("Some diff");
+        System.out.println("No Crackers means, No Pollution");
     }
 }
